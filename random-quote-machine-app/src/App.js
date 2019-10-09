080b432c8c83fb4ea6408fb1fbe93fbf26460b61
Mikie Smit <msmit1123@gmail.com>
2019-10-08 20:07:11 -0500

    static build of site version

--- random-quote-machine-app/src/App.js
+++ random-quote-machine-app/src/App.js
@@ -1,24 +1,44 @@
+/**
+ * Import Node Dependencies
+ */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+/**
+ * Import other dependencies
+ */
+//CSS
+import './App.scss'
+
+/**
+ * Import Components
+ */
+import Button from './components/Button'
+
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* Include Grading Script for Free Code Camp */}
+    <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+      <header>
+        <h1>Random Quote Machine</h1>
+        view: <span>website | SWITCH | footer</span>
       </header>
+      <div id="quote-box">
+        <div id="text">
+          "You miss 100% of the shots you don't take" -Wayne Gretzky
+        </div>
+        <div id="author">
+          -Michael Scott
+        </div>
+        <div id="button-container">
+          <Button id="new-quote">New Quote</Button> 
+          <Button><a href="www.google.com" id="tweet-quote">Tweet</a></Button>
+        </div>
+      </div>
+
     </div>
   );
 }
--- random-quote-machine-app/src/index.js
+++ random-quote-machine-app/src/index.js
@@ -1,12 +1,19 @@
+/**
+ * Import Node Dependencies
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+/**
+ * Import other Dependencies
+ */
+//CSS
 import './index.css';
+
+/**
+ * Import Components
+ */
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+//Render App to root
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

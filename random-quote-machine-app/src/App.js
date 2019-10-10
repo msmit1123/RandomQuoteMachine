47b125f81b6b9764f7d628daa16fa08af9dcf465
Mikie Smit <msmit1123@gmail.com>
2019-10-10 11:05:01 -0500

    added random quote functionality and quote bank

--- random-quote-machine-app/src/App.js
+++ random-quote-machine-app/src/App.js
@@ -18,12 +18,29 @@ import Button from './components/Button';
 import ToggleSwitch from './components/ToggleSwitch';
 
 
+//Define quote bank
+var quoteBank = [
+  { quote: "All that is gold does not glitter, Not all those who wander are lost, The old that is strong does not wither, Deep roots are not reached by the frost.", author: "JRR Tolkien" },
+  { quote: "'You miss 100% of the shots you don't take - Wayne Gretzky", author: "Michael Scott" },
+  { quote: "Treat every list like it's a list of sandwiches.", author: "Mikie"},
+  { quote: "There are only two professions condemned in the Bible: being satan, and being a tax collector. Frankly, I'm not sure which is worse.", author: "John" },
+  { quote: "Price is what you pay. Value is what you get.", author: "Ancient Proverb" },
+  { quote: "Bacon is the cheese of meats.", author: "John" },
+  { quote: "A ravioli is just an Italian potsticker.", author: "John" },
+  { quote: "Have you ever been in a submarine that lost its ability to surface and you're stuck inside it sinking to the ocean floor as the water pressure slowly crushes the sub around you and you hear the metal creaking and groaning as you approach your certain doom? It's like that.", author: "John" },
+  { quote: "I don't want to be the idiot stuck holding the goose when the chicken man comes to town", author: "Mikie" },
+  { quote: "It was just as insulting whether you heard it or not", author: "Kevin" },
+  { quote: "The law is a shield, not a sword. Those who attempt to wield it as a sword deserver to be struck by it", author: "Mikie" },
+  { quote: "There is a white haired old man cutting wood in your yard.", author: "Bob" }
+]
 
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      isFooterChecked: false
+      isFooterChecked: false,
+      quote: "",
+      author: "",
     }
     this.displayAsFooter = this.displayAsFooter.bind(this);
     this.displayAsSite = this.displayAsSite.bind(this);
@@ -48,7 +65,12 @@ class App extends React.Component{
   }
 
   newQuote(){
-    this.newText();
+    var randIndex = Math.floor(Math.random()*quoteBank.length)
+    this.setState({
+      quote: quoteBank[randIndex].quote,
+      author: quoteBank[randIndex].author
+    })
+
     this.newColor();
   }
 
@@ -78,11 +100,11 @@ class App extends React.Component{
 
         <div id="quote-box" className={curView}>
           <div id="text" className={curView}>
-            "You miss 100% of the shots you don't take" -Wayne Gretzky
+            "{this.state.quote}"
           </div>
           
           <div id="author" className={curView}>
-            -Michael Scott
+            -{this.state.author}
           </div>
 
           <div id="button-container" className={curView}>
